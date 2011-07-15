057900d6d07bf3d70ec6c4ca3eaf853eff44d38c
apla <dot.and.thing@gmail.com>
2011-07-15 20:48:47 +0400

    added asis renderer

--- RIA/Workflow/renderTask.js
+++ RIA/Workflow/renderTask.js
@@ -18,11 +18,14 @@ common.extend (renderTask.prototype, {
 		
 		if (this.type == 'json') {
 			self.output.setHeader("Content-Type", "text/json; charset=utf-8");
-//			self.emit ('log', 'data: ' + JSON.stringify(self.data));
-//			self.emit ('log', 'out: ' + JSON.stringify(self.output));
 			self.output.end (JSON.stringify(self.data));
 			self.completed ();
+		} else  if (this.type == 'asis') {
+			self.output.setHeader ("Content-Type", self.contentType);
+			self.output.end (self.data);
+			self.completed ();
 		}
+
 		
 	}
 });
