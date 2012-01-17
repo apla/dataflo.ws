0c28f08d6edccf4eaf7c70f2dc7f71981db05c77
apla <dot.and.thing@gmail.com>
2012-01-17 15:32:11 +0400

    added example

---
+++ example/bin/httpd.js
@@ -0,0 +1,44 @@
+#!/usr/bin/env node
+
+var common  = require ('common');
+var httpdi  = require ('initiator/http');
+var util    = require ('util');
+var barrier = require ('barrier');
+var urlUtil = require ('url');
+var workflow = require ('workflow');
+
+module.exports = {
+	generateSomething: function (params) {
+		return {
+			receivedParamA: params.paramA,
+			receivedParamB: params.paramB
+		}
+	}
+};
+
+project.on ('ready', function () {
+	
+	var config = {
+		port: 50088,
+		static: {
+			index: "index.html",
+			root: project.root
+		},
+		workflows: [{
+			url: "/data",
+			tasks: [{
+				functionName: "generateSomething",
+				paramA: "111",
+				paramB: "222",
+				produce: "data.something"
+			}],
+			presenter: {
+				type: "json",
+				vars: "{$data.something}"
+			}
+		}]
+	};
+
+	var initiator = new httpdi (config);
+
+});
