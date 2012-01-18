8ad2675c3c8ced76277c6f1289fb092480742b2a
xoxulin <a.hohulin@gmail.ru>
2012-01-18 12:00:12 +0400

    req.session

--- task/cookieSession.js
+++ task/cookieSession.js
@@ -15,25 +15,33 @@ util.extend (sessionGenerator.prototype, {
 		
 		var self = this;
 		
+		var stoken;
+		
 		var cookies = self.cookies;
 		
-		if (cookies.stoken) {
-			self.completed(cookies.stoken);
-			return;
+		if (cookies.stoken) { // if stoken exist
+			
+			stoken = cookies.stoken;
+		
+		} else { // generate
+		
+			var secret = self.secret;
+			var ip = self.request.connection.remoteAddress;
+			var port = self.request.connection.remotePort;
+			
+			var date = new Date();
+			var rnd = ~~(10e+9*Math.random());
+			
+			var stokenStr = secret + ':' + ip + ':' + '.' + date.getTime() + '.' + rnd;
+			stoken = new Buffer(stokenStr).toString('base64').replace(/=+$/, '');
 		}
 		
-		// - - - - - if no stoken - generate it
+		// ---
 		
-		var secret = self.secret;
-		var ip = self.request.connection.remoteAddress;
-		var port = self.request.connection.remotePort;
+		self.request.session = {stoken: stoken};
 		
-		var date = new Date();
-		var rnd = ~~(10e+9*Math.random());
+		// ---
 		
-		var stokenStr = secret + ':' + ip + ':' + '.' + date.getTime() + '.' + rnd;
-		var stoken = new Buffer(stokenStr).toString('base64').replace(/=+$/, '');
-		
-		self.completed (stoken)
+		self.completed (stoken);
 	}
 });
