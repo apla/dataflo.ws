0ea44b996d7b3e514c9a39c963ec94fa9d99941b
apla <dot.and.thing@gmail.com>
2012-07-26 12:15:25 +0400

    fixed value override issue

--- task/cookie.js
+++ task/cookie.js
@@ -104,11 +104,13 @@ util.extend (cookieParser.prototype, {
 			
 		// - - -
 		
-		var newCookie = {value: value}
+		var newCookie = {};
 		
 		for (var key in cookieTpl) {
 			newCookie[key] = cookieTpl[key];
 		}
+		
+		newCookie[value] = value;
 		
 		self.completed (newCookie);
 	},
