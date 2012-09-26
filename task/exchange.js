bef15ddea29b9eb2053a132052bec80616de1b6a
Alex Petrov <petroff.alex@gmail.com>
2012-09-26 21:02:42 +0400

    autosuggest from echange

--- task/exchange.js
+++ task/exchange.js
@@ -3,11 +3,12 @@ var task = require('task/base'),
 	https = require('https'),
 	url	= require('url'),
 	io = require ('io/easy'),
-	crypto = require ('crypto');
+	crypto = require ('crypto'),
+	crack = require("crack");
 
 var exchangeConfig = project.config.consumerConfig.exchange;
-
 var wsdlUrl = exchangeConfig.wsdlUrl;
+var servicesUrl = exchangeConfig.servicesUrl;
 
 var exchange = module.exports = function (config) {
 	this.init (config);	
@@ -135,6 +136,157 @@ util.extend(exchange.prototype, {
 			
 			self.completed({
 				accessAllowed: true
+			});
+			
+		}
+	},
+
+	tmpl: function (str, obj) {
+		return str.replace(
+			/{\$(.+?)}/g,
+			function (_, key) { return obj[key]; }
+		);
+	},
+
+	prepareQuery: function () {
+		var queryTpl = [
+			'<?xml version="1.0" encoding="utf-8"?>',
+			'<soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/" xmlns:t="http://schemas.microsoft.com/exchange/services/2006/types">',
+			'<soap:Body>',
+			'<ResolveNames xmlns="http://schemas.microsoft.com/exchange/services/2006/messages" xmlns:t="http://schemas.microsoft.com/exchange/services/2006/types" ReturnFullContactData="true">',
+			'<UnresolvedEntry>{$filter}</UnresolvedEntry>',
+			'</ResolveNames>',
+			'</soap:Body>',
+			'</soap:Envelope>'
+		].join(' ');
+
+
+	},
+
+	searchUsers: function () {
+		var self = this,
+			user = self.user;
+
+		if (user && user.authType == 'exchange' && user.tokens && user.tokens.login && user.tokens.password) {
+			
+			var login    = user.tokens.login,
+				password = this.decode(user.tokens.password);
+			
+			var options = url.parse(servicesUrl);
+
+			var queryTpl = [
+				'<?xml version="1.0" encoding="utf-8"?>',
+				'<soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/" xmlns:t="http://schemas.microsoft.com/exchange/services/2006/types">',
+				'<soap:Body>',
+				'<ResolveNames xmlns="http://schemas.microsoft.com/exchange/services/2006/messages" xmlns:t="http://schemas.microsoft.com/exchange/services/2006/types" ReturnFullContactData="true">',
+				'<UnresolvedEntry>{$filter}</UnresolvedEntry>',
+				'</ResolveNames>',
+				'</soap:Body>',
+				'</soap:Envelope>'
+			].join(' ');
+
+			var query = this.tmpl(queryTpl, this.pager);
+
+			options.auth = login+':'+password;
+			options.port = 443;
+			options.headers = { 
+				'Content-Type': 'text/xml'
+			};
+			options.method = 'POST';
+					
+			var req = https.request(options, function (response) {
+				var exchangeXmlAnswer = [];
+
+				response.setEncoding('utf-8');
+				response.on('data', function(data) {
+				  exchangeXmlAnswer.push(data);
+				});
+
+				response.on('end', function() {
+					var error = '';
+
+					exchangeXmlAnswer = exchangeXmlAnswer.join('');
+					var crackedResponse = crack(exchangeXmlAnswer);
+					var objResponse = crackedResponse.toJS();
+
+					if(objResponse.Body.Fault) {
+				        console.log('!!!!!!!!!!!!!!! RESPONSE FAULT:', objResponse.Body.Fault.faultstring.__content);
+				        self.failed({
+							statusCode: 500,
+							msg: 'Response Fault!'
+						});
+				        return;
+				    }
+
+				    var objResolveNamesResponseMessage = objResponse.Body.ResolveNamesResponse.ResponseMessages.ResolveNamesResponseMessage;
+
+				    if(objResolveNamesResponseMessage.ResponseClass != 'Success' && objResolveNamesResponseMessage.ResponseClass != 'Warning'){
+				        console.log('!!!!!!!!!!!!!!! NOT SUCCESS!');
+				        self.failed({
+							statusCode: 500,
+							msg: 'Response Not Success!'
+						});
+						error = 'Response Not Success!';
+				        //return;
+				    }
+
+				    var users = [];
+
+				    var objResolutionSet = objResolveNamesResponseMessage.ResolutionSet;
+				    if(Object.prototype.toString.call( objResolutionSet ) === '[object Array]') {
+				    	console.log('Found: '+objResolutionSet.TotalItemsInView);
+				    	console.log('Query:', self.pager.filter);
+					}
+				    if(Object.prototype.toString.call( objResolutionSet.Resolution ) === '[object Array]') {
+				        objResolutionSet.Resolution.forEach(function(objResolution){
+				            var objMailbox = objResolution.Mailbox;
+				            var objContact = objResolution.Contact;
+				            users.push({
+				            	name: objMailbox.Name,
+							    link: undefined,
+							    authType: 'exchange',
+							    avatar: '',
+							    email: objMailbox.EmailAddress,
+							    _id: objMailbox.EmailAddress,
+							    text: objMailbox.Name
+				            });
+				            //console.log('Name: ' + objMailbox.Name);
+				            //console.log('Email: ' + objMailbox.EmailAddress);
+				        });
+				    } else if (Object.prototype.toString.call( objResolutionSet.Resolution ) === '[object Object]') {
+				    	users.push({
+			            	name: objResolutionSet.Resolution.Mailbox.Name,
+						    link: undefined,
+						    authType: 'exchange',
+						    avatar: '',
+						    email: objResolutionSet.Resolution.Mailbox.EmailAddress,
+						    _id: objResolutionSet.Resolution.Mailbox.EmailAddress,
+						    text: objResolutionSet.Resolution.Mailbox.Name
+			            });
+				        //console.log('Name: ' + objResolutionSet.Resolution.Mailbox.Name);
+				        //console.log('Email: ' + objResolutionSet.Resolution.Mailbox.EmailAddress);
+				    }
+
+					self.completed({
+						data: users || null,
+						total: users ? users.length : 0,
+						success: !error,
+						error: error
+					});
+				});
+			});			
+			req.on('error', function(e) {
+			  console.error('!!!!!!!!!!!!!', e);
+			});
+
+			
+			req.write(query);
+			req.end();
+			
+		} else {
+			
+			self.completed({
+				
 			});
 			
 		}
