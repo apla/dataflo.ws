145888e335c1b0cf35af9c59562730d734cff540
xoxulin <alex.hohulin@gmail.com>
2012-12-13 19:51:53 +0400

    repl fix

--- repl.js
+++ repl.js
@@ -2,7 +2,6 @@ var repl   = require("repl");
 var common = require ("./common");
 
 repl.start({
-  prompt: "node via stdin> ",
   input: process.stdin,
   output: process.stdout
 }).context.project = global.project;
