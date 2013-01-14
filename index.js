3e459ac7ec913dc4054eefcebc620ad0ad86e7f1
katspaugh <ivan.kalyaev+github@gmail.com>
2013-01-14 18:36:58 +0400

    add index.js for symlinking modules

---
+++ index.js
@@ -0,0 +1,38 @@
+var path = require('path');
+var fs = require('fs');
+
+var directories = [
+	'initiator',
+	'task',
+	'model',
+	'io'
+];
+
+/**
+ * Makes symlinks from modules to base dataflo.ws directory.
+ */
+module.exports = function (moduleName) {
+	var baseDir = path.dirname(require.resolve('dataflo.ws'));
+	var moduleDir = baseDir + '-' + moduleName;
+
+	directories.forEach(function (dir) {
+		var srcDir = path.join(moduleName, dir);
+		var destDir = path.join(baseDir, dir);
+
+		if (fs.existsSync(srcDir)) {
+			var files = fs.readdirSync(srcDir);
+
+			if (files.length) {
+				if (!fs.existsSync(destDir)) {
+					fs.mkdirSync(destDir);
+				}
+
+				files.forEach(function (srcPath) {
+					var fileName = path.basename(srcPath);
+					var destPath = path.join(destDir, fileName);
+					fs.symlinkSync(srcPath, destPath);
+				});
+			}
+		}
+	});
+};
