2181cc0873bb3f7363632a07911e0b8bd572389d
katspaugh <ivan.kalyaev+github@gmail.com>
2013-02-05 16:12:14 +0400

    rm obsolete stuff; improve example

--- repl.js
+++
@@ -1,7 +0,0 @@
-var repl   = require("repl");
-var common = require ("./common");
-
-repl.start({
-  input: process.stdin,
-  output: process.stdout
-}).context.project = global.project;
--- test.js
+++
@@ -1,7 +0,0 @@
-var makeCounter = function (x) {
-	return function () {
-		return (x += 1);
-	};
-};
-
-var counter = makeCounter(0);
