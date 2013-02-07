7f5292ead92b36f731b5fa9f16427d3afc2d5f1f
katspaugh <ivan.kalyaev+github@gmail.com>
2013-02-07 20:00:24 +0400

    cleanup whitespace

--- index.js
+++ index.js
@@ -21,9 +21,9 @@ module.exports = {
 	/**
 	 * Makes symlinks from modules to base dataflo.ws directory.
 	 */
-	
+
 	install: function (moduleName) {
-		
+
 		var baseDir = path.dirname(require.resolve(MODULE_NAME));
 		var nodePath = path.dirname(baseDir);
 		var moduleDir = path.join(nodePath, moduleName);
@@ -43,68 +43,68 @@ module.exports = {
 				});
 			}
 		});
-		
+
 	},
-	
+
 	/**
 	 * Register base entities for dataflo.ws processing.
 	 */
-	
+
 	register: function(instanceType, instanceName, instanceClass) {
-		
+
 		if (!registry[instanceType]) {
-			
+
 			console.warn(
 				'Unexpected instance type. Predefined types [' +
 				instanceTypes.join(', ') +
 				']'
 			);
-			
+
 			return;
 		}
-		
+
 		registry[instanceType][instanceName] = instanceClass;
-		
+
 	},
-	
+
 	/**
 	 * Get base entities for dataflo.ws processing from reqister or FS.
 	 */
-	
+
 	getEntityClass: function(instanceType, instanceName) {
-		
+
 		var instanceClass = registry[instanceType] && registry[instanceType][instanceName];
-		
+
 		if (!instanceClass) {
-		
+
 			// get from symlink
 			try {
-				
+
 				instanceClass = require(
 					path.join(MODULE_NAME, instanceType, instanceName
 				));
-				
+
 			} catch (e) {
-				
+
 				// FIXME later: legacy initiator names
 				var fixedName = instanceName.replace(/d$/, '');
-				
+
 				if (fixedName !== instanceName) {
-					
+
 					console.warn(
 						'[DEPRECATED] Remove trailing "d" from "%s" in your initiator config',
 						instanceName
 					);
 				}
-				
+
 				throw e;
-				
+
 			}
 		}
-		
+
 		return instanceClass;
-		
+
 	},
-	
+
 	common: common
 };
