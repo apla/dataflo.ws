bb4811463331747dd91abcc7bd8cfab8f9f2d4e7
jrad <behradz@gmail.com>
2013-06-23 09:42:10 +0430

    Use falsy values for $empty

--- workflow.js
+++ workflow.js
@@ -346,7 +346,8 @@ var workflow = module.exports = function (config, reqParam) {
 							if (!failed) {
 								this.completed(returnVal);
 
-								if (isVoid(returnVal)) {
+//								if (isVoid(returnVal)) {
+								if (common.isEmpty(returnVal)) {
 									this.empty();
 								}
 							}
