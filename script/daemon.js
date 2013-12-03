b8168cae963f9682f1f790f3878e6e22dec49410
apla <dot.and.thing@gmail.com>
2013-12-03 17:40:39 +0400

    simplified daemons enum; version up

--- script/daemon.js
+++ script/daemon.js
@@ -9,11 +9,16 @@ module.exports = {
 	},
 	launch: function (conf) {
 		var daemonName = this.launchContext().configKey;
+		var configDaemonNames = Object.keys(conf.daemon);
+		if (daemonName == undefined && configDaemonNames.length == 1)
+			daemonName = configDaemonNames[0];
 		if (!conf.daemon || !conf.daemon[daemonName]) {
 			// TODO: add description for daemon config generation
 			console.error(
 				'No daemon named "%s" found in configuration', daemonName
 			);
+			var logDaemonNames = configDaemonNames.join ('", "');
+			console.error ('You can select one from those daemon configurations: "%s"', logDaemonNames);
 			process.exit();
 		}
 		var daemonConf = conf.daemon[daemonName];
