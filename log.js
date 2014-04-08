8127b27869b17e028338fedd43cf1a581535cfb0
apla <dot.and.thing@gmail.com>
2014-04-08 20:20:01 +0400

    no more ansi-color

--- log.js
+++ log.js
@@ -1,25 +1,41 @@
-var color;
+var util   = require ('util');
+var common = require ('./common');
 
-try {
-	ansiColor = require("ansi-color").set;
-	color = function (color) {
-		var args = [].slice.apply (arguments);
-		args.shift();
-		var msg = args.join (' ');
-		return ansiColor (msg, color);
-	}
-} catch (e) {
-	color = function (color) {
-		var args = [].slice.apply (arguments);
-		args.shift();
-		var msg = args.join (' ');
-		return msg;
-	}
-} finally {
-	'black red green yellow blue magenta cyan white'.split (' ').forEach (function (colorName) {
-		color[colorName] = color.bind (color, colorName);
-	});
+var addedColors = {
+	"black_bg": 40,
+	"red_bg": 41,
+	"green_bg": 42,
+	"yellow_bg": 43,
+	"blue_bg": 44,
+	"magenta_bg": 45,
+	"cyan_bg": 46,
+	"white_bg": 47
+};
+
+for (var colorAdd in addedColors) {
+	util.inspect.colors[colorAdd] = [addedColors[colorAdd], 0];
 }
+
+function color (color) {
+	if (!color || $isClientSide)
+		return str;
+	var color_attrs = color.split("+");
+	var ansi_str = "";
+	for (var i=0, attr; attr = color_attrs[i]; i++) {
+		ansi_str += "\033[" + util.inspect.colors[attr][0] + "m";
+	}
+
+	var args = [].slice.apply (arguments);
+	args.shift();
+	var str = args.join (' ');
+
+	ansi_str += str + "\033[" + 0 + "m";
+	return ansi_str;
+}
+
+'black red green yellow blue magenta cyan white'.split (' ').forEach (function (colorName) {
+	color[colorName] = color.bind (color, colorName);
+});
 
 var log = {};
 
