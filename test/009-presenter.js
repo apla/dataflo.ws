55d1c42eb803396923ef71084e57b1f5820d2391
apla <dot.and.thing@gmail.com>
2015-11-24 00:26:57 +0300

    added base for presenter test

---
+++ test/009-presenter.js
@@ -0,0 +1,51 @@
+var testCommon = require ("../test/common");
+testCommon.injectMain ();
+
+var baseName = testCommon.baseName (__filename);
+
+var httpi     = require ("../initiator/http");
+var flow      = require ("../flow");
+
+var dataflows = require ("../");
+
+var paint = dataflows.color;
+
+var testData = testCommon.initTests (__dirname, baseName);
+
+var verbose = false;
+
+var httpDaemon;
+
+var descriptor = {
+
+	before: function (done) {
+		// runs before all tests in this block
+		httpDaemon = new httpi (testData.initiator.http, {
+			logger: function () {
+				var toLog = [].slice.call (arguments);
+				var level = toLog.shift() || 'log';
+
+				toLog = this.buildLogString.apply (this, toLog);
+
+				// we don't need date here
+				toLog[0] = "|http daemon|";
+
+				toLog = paint.fillUnpainted ('grey', toLog.join (' '));
+
+				(console[level] || console.log).call (console, toLog);
+
+			}
+		});
+		httpDaemon.on ('ready', done);
+	},
+	after: function (done) {
+		// runs after all tests in this block
+		httpDaemon.server.close (done);
+	}
+};
+
+
+describe (baseName + " running presenter tests", testCommon.runTests.bind (descriptor, testData, {
+	// dataflow parameters
+	initiator: testData.initiator // for host name and port
+}, verbose));
